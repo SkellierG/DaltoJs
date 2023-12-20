8577ce96d6eb9324e368243beda4c78cf01ed031
Benjamin Gonzalez <benja314gonzalez@gmail.com>
2023-12-20 19:58:19 -0300

    console commands

---
+++ Junior/consola/JS23_00/script.js
@@ -0,0 +1,20 @@
+ //utilities 
+
+let br = `<br><br>`;
+
+//	division
+
+//for (let i = 0; i < 6; i++) {
+//	document.write(br);
+//}
+
+let array = ["primero", "Segundo", "tercero", 1, 2 ,3];
+
+console.assert(2 == 1);
+console.assert(2 == 2);
+console.warn("Esto es una alerta");
+console.error("Esto es un error");
+console.log("console.info vvvv");
+console.info(array);
+console.log("Esto es un log vvvv");
+console.log(array);
