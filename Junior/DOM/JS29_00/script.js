a710532a9b677078436935025d5af330f6079c6f
Benjamin Gonzalez <benja314gonzalez@gmail.com>
2024-01-12 12:51:20 -0300

    JS29

--- Junior/DOM/JS29_00/script.js
+++ Junior/DOM/JS29_00/script.js
@@ -16,3 +16,33 @@ console.log(input.className);
 console.log(input.value);
 console.log(input.type = "file");
 console.log(input.accept = "image/jpg");
+
+const formulario = document.getElementById("input-formulario");
+
+formulario.minLength = 5;
+formulario.maxLength = 10;
+formulario.placeholder = "escribime pelotudo";
+formulario.required = "true";
+
+const tercero = document.querySelectorAll(".tercero");
+
+tercero[1].style = "color: orange";
+tercero[2].style = "color: orange";
+tercero[1].style.backgroundColor = "blue";
+tercero[2].style.backgroundColor = "#222";
+
+//
+
+const cuarto = document.querySelectorAll(".cuarto");
+cuarto[1].classList.add("rojaso");
+cuarto[2].classList.add("rojaso");
+
+const quinto = document.querySelectorAll(".quinto");
+quinto[1].classList.add("verdaso");
+quinto[2].classList.add("verdaso");
+quinto[1].classList.add("rojaso");
+quinto[2].classList.add("rojaso");
+quinto[1].classList.remove("rojaso");
+quinto[2].classList.remove("rojaso");
+
+
